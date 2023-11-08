f1db85060925662b81fff4ca3a5674e10c8e186e
Andy Simmons <rocknswimmer116@gmail.com>
2023-11-08 14:39:57 -0500

    thoughts for fields

--- src/Form.jsx
+++ src/Form.jsx
@@ -5,6 +5,8 @@ function IntForm() {
   return (
     <div>
       <input placeholder="name"></input>
+      <input placeholder="email"></input>
+      <input placeholder="connection?"></input>
     </div>
   )
 }
