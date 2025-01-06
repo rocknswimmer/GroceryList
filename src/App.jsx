90020afe063999250bd8d4e929596484838a0b21
Andy Simmons <rocknswimmer116@gmail.com>
2025-01-06 12:11:58 -0500

    clean up for future projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import IntForm from './Form.jsx';
+
 
 function App() {
 
   return (
     <div>
-      <h1>Waileys SignUp</h1>
-      <IntForm />
+      <h1>Starter Code</h1>
     </div>
   )
 }
