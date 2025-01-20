e21dc40f5bef3d4db968e8112558b703d063f99a
Andy Simmons <rocknswimmer116@gmail.com>
2025-01-19 19:25:28 -0500

    seed file created

--- server/index.js
+++ server/index.js
@@ -4,6 +4,17 @@ const app = express();
 const compression = require('compression')
 const path = require('path');
 
+require('dotenv').config();
+
+const Pool = require('pg').Pool;
+const pool = new Pool({
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  port: process.env.PGPORT,
+  password: process.env.PGPASSWORD // this was a hard typo to find in the terminal
+});
+
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
