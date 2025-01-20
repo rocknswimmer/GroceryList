9c06b36b3bdf9783af8bb73565e89fd683ff1a21
Andy Simmons <rocknswimmer116@gmail.com>
2025-01-19 19:38:04 -0500

    added modal and final thoughts for the day

--- src/Inventory.jsx
+++ src/Inventory.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import InventoryItem from './InventoryItem.jsx';
 import Footer from './Footer.jsx';
+import Modal from './modal.js';
+import axios from 'axios';
+
 
 const Inventory = () => {
+
+  // need function for each button and for each modal opened by a button if that is how the button will work
 
 
 
