028772d93cecfb662b67878901c6147718aeb776
Andy Simmons <rocknswimmer116@gmail.com>
2025-01-23 18:31:06 -0500

    mapping get data

--- src/GroceryItem.jsx
+++ src/GroceryItem.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const GroceryItem = () => {
+const GroceryItem = ({item}) => {
+//item object {id: 6, user_num: 7, item: 'Post and Get Test', quantity: 3, units: 'Get Issue'}
+//console.log(item);
 
-
-
-  return(<h2>Grocery List Item</h2>)
+  return(<h2>{item.item} {item.quantity} {item.units} </h2>)
 }
 
 export default GroceryItem;
