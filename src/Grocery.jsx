3d80334fcfa964f6eb0d75264798cf39d1151bdf
Andy Simmons <rocknswimmer116@gmail.com>
2025-01-24 12:00:10 -0500

    delete comment after completed

--- src/Grocery.jsx
+++ src/Grocery.jsx
@@ -26,7 +26,7 @@ const Grocery = ({groceryList, updateGL}) => {
       {groceryList.length == 0 && <p>No Items Currently On Your Grocery List</p>}
       {viewAdd && <Modal close={()=>{setViewAdd(false)}} content={<AddGIForm close={()=>{setViewAdd(false)}} update={()=>{updateGL()}} />}/>}
 
-      {/* need to pass item number!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! */}
+
       {viewUpdate && <Modal close={()=>{setViewUpdate(false)}} content={<UpdateGIForm item={itemObj} close={()=>{setViewUpdate(false)}} update={()=>{updateGL()}} />} />}
 
       {viewDelete && <Modal close={()=>{setViewDelete(false)}} content={<DeleteGIForm item={itemObj}  close={()=>{setViewDelete(false)}} update={()=>{updateGL()}}/>}  />}
