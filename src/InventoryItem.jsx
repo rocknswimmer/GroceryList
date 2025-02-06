faae0e1f446f0a267ffa3ac9f37c753e270ea6af
Andy Simmons <rocknswimmer116@gmail.com>
2025-02-05 20:26:34 -0500

    basic inventory grid done

--- src/InventoryItem.jsx
+++ src/InventoryItem.jsx
@@ -9,7 +9,13 @@ const InventoryItem = ({item, viewUpdate, updateItemObj, updateII, viewDelete, d
       {viewUpdate && <button onClick={() => {updateItemObj(item);updateII()}}>Update This Item</button>}
       {viewDelete && <button onClick={() => {updateItemObj(item);deleteII()}} className="deletee">Delete This Item</button>}
       {viewAddToGL && <button onClick={() => {updateItemObj(item);addToGL()}}>Add Item To Grocery List</button>}
-      <span>{item.item} {item.quantity} {item.units} {item.item_location} {item.expires}</span>
+      <div className='icontainer'>
+        <div className="ii">{item.item}</div>
+        <div className="ii">{item.quantity}</div>
+        <div className="ii">{item.units}</div>
+        <div className="ii">{item.item_location}</div>
+        <div className="ii">{item.expires}</div>
+      </div>
     </div>
   )
 }
